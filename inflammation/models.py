Bugfix normalisation function

inflammation/models.py
@@ -53,8 +53,6 @@ def patient_normalise(data):
 
     Negative values are rounded to 0.
     """
-    if np.any(data < 0):
-        raise ValueError('Inflammation values should not be negative')
     if not isinstance(data, np.ndarray):
         raise TypeError('Inflammation data is not a numpy array, please check the format of the data')
     data_shape = data.shape
@@ -62,6 +60,8 @@ def patient_normalise(data):
         raise TypeError('Inflammation data array does not have the right shape, should be 2D array')
     if (data_shape[0] <= 0 and data_shape[1] <= 0):
         raise TypeError('Inflammation data array does not have the right shape, should be 2D array')
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
     max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max[:, np.newaxis]